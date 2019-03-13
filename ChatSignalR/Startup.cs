f402a3e4a416c18226d282a6662c2b4309360098
Weicheng Xu <WX56494@worksafebc.com>
2019-03-13 14:31:04 -0700

    initial

--- ChatSignalR/Startup.cs
+++ ChatSignalR/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ChatSignalR.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -32,6 +33,8 @@ namespace ChatSignalR
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,6 +58,10 @@ namespace ChatSignalR
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            app.UseSignalR(routes =>
+                routes.MapHub<ChatHub>("/chathub")
+            );
         }
     }
 }
---
+++ ChatSignalR/wwwroot/js/chat.js
@@ -0,0 +1,26 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+$("#sendButton").attr("disabled","disabled");
+
+connection.on("ReceiveMessage", function (user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = user + " says " + msg;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
+
+connection.start().then(function () {
+        $("#sendButton").removeAttr("disabled");
+    })
+    .catch(function (err) {
+    return console.error(err.toString());
+    })
+
+$("#sendButton").click(function (event) {
+    event.preventDefault();
+    connection.invoke("SendMessage", $("#userInput").val(), $("#messageInput").val())
+        .catch(function (err) { return console.error(err.toString()) });
+})
